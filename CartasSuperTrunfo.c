88b290b762c575d9c3556bec3f3cf06a64b56f82
Biel35 <83042989+Gabrico45@users.noreply.github.com>
2025-04-04 19:55:14 +0000

    Super Trunfo iniciante

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,41 +1,49 @@
 #include <stdio.h>
 
-int main (){
+int main() {
+    char codigo[4]; // Código da carta (ex: A01)
+    int populacao;  // População da cidade
+    float area;     // Área da cidade
+    double pib;     // PIB da cidade
+    int pontosTuristicos; // Número de pontos turísticos
+    char continuar = 's'; // Controle para cadastrar outra carta
+
+    printf("Bem-vindo ao Desafio Super Trunfo - Países!\n");
+    printf("Cadastro de cartas com informações das cidades.\n");
+
+    do {
+        // Solicitar o código da carta ao usuário
+        printf("\nDigite o código da carta (exemplo: A01): ");
+        scanf("%s", codigo);
+
+        // Entrada de dados
+        printf("Digite a população da cidade: ");
+        scanf("%d", &populacao);
+
+        printf("Digite a área da cidade (em km²): ");
+        scanf("%f", &area);
+
+        printf("Digite o PIB da cidade (em bilhões): ");
+        scanf("%lf", &pib);
+
+        printf("Digite o número de pontos turísticos: ");
+        scanf("%d", &pontosTuristicos);
+
+        // Exibição das informações cadastradas
+        printf("\n--- Carta cadastrada: %s ---\n", codigo);
+        printf("População: %d habitantes\n", populacao);
+        printf("Área: %.2f km²\n", area);
+        printf("PIB: %.2f bilhões\n", pib);
+        printf("Pontos turísticos: %d\n", pontosTuristicos);
+
+        // Perguntar se deseja cadastrar outra carta
+        printf("\nDeseja cadastrar outra carta? (s/n): ");
+        scanf(" %c", &continuar);
+
+    } while (continuar == 's' || continuar == 'S');
     
-    int populacao;
-    float area;
-    float PIB;
-    int turisticos;
-    char nome[50];
-    char pais[50];
 
-    printf("Digite o nome do país:\n");
-    printf("- Brasil\n");
-    printf("- Japão\n");
-    scanf("%s", pais);
-
-    printf("Escolha sua população: \n");
-    scanf("%d", &populacao);
-
-    printf("Area em km²: \n");
-    scanf("%f", &area);
-
-    printf("PIB: \n");
-    scanf("%f", &PIB);
-    
-    printf("Número de pontos turísticos: \n");
-    scanf("%d", &turisticos);
-    
-    printf("%s é o país escolhido\n", pais);
-    printf("A população do %s é: %d\n", pais, populacao);
-    printf("A área do %s é: %.2f km²\n", pais, area);
-    printf("O PIB do %s é: %.2f\n", pais, PIB);
-    printf("O número de pontos turísticos do %s é: %d\n", pais, turisticos);
+    printf("\nCadastro concluído! Obrigado por usar o sistema.\n");
 
     return 0;
-
-
-
-
-
-}
+}    

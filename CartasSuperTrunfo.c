7f9e6e2212b3de6ca0262ea3979d631748e9cad2
Biel35 <83042989+Gabrico45@users.noreply.github.com>
2025-04-06 16:44:01 -0300

    Desafio: nível aventureiro CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,59 +2,93 @@
 
 int main(){
     
-    int populacao;  // População do país
-    float area;  // Área do país em km²
-    double PIB;   // PIB do país em bilhões
+    int populacao;  // População da cidade
+    float area;  // Área da cidade em km²
+    double PIB;   // PIB da cidade em bilhões
     int pontosturisticos;  // Número de pontos turísticos
     char codigo[4];  //código da carta, com limite de 4 caracteres.
     char pais[20];  // Nome do país, com limite de 20 caracteres.
-    char continuar = 's'; // Vai captar se o usuario quer cadastrar outra carta ou não.
+    char cidade[20]; // Nome da cidade, com limite de 20 caracteres.
+    char estado[20]; // Nome do estado, com limite de 20 caracteres.
     
-    do { //enquanto tiver no do e while, o programa vai rodar.
-        printf("Bem vindo ao Super Trunfo! Iniciado cadastramento das cartas...\n");
-        printf("\nDigite o codigo país (exemplo: A01): ");
-        scanf("%s", codigo); // %s é para ler string, e o & é para ler o endereço de memoria.
+     
+    printf("Bem vindo ao Super Trunfo! Iniciado cadastramento das cartas...\n");
+    printf("\nDigite o codigo país (exemplo: A01): ");
+    scanf("%s", codigo); // %s é para ler string, e o & é para ler o endereço de memoria.
 
-        printf("Digite o nome do país: ");
-        scanf("%s", pais);  
-
-        printf("Digite a população do país : ");
-        scanf("%d", &populacao);
-
-        printf("Digite a área do país em km²: ");
-        scanf("%f", &area);
-
-        printf("Digite o PIB do país em bilhões: ");
-        scanf("%lf", &PIB);
-
-        printf("Digite o número de pontos turísticos do país: ");
-        scanf("%d", &pontosturisticos);
-
-        printf("\nCarta cadastrada!\n");
-
-        printf("Seu Páis é: %s\n", pais);
-        printf("Codigo do país: %s\n", codigo);
-        printf("População: %d\n", populacao);
-        printf("Área: %.2f km²\n", area);
-        printf("PIB: %.2lf Bilhões\n", PIB);
-        printf("Pontos turísticos: %d\n", pontosturisticos);
-
-        printf("Obrigado por cadastrar sua carta!\n");
-        printf("Deseja cadastrar outra carta? (s/n): ");
-        scanf(" %c", &continuar);
-    }   while (continuar == 's' || continuar == 'S'); //while enquanto o usuario disser "s" ou "S" ele vai voltar no inicio para cadastrar outra carta, se o usuario disser não "n" o programa sai do while e finaliza.
+    printf("Digite o nome do país: ");
+    scanf("%s", pais);
         
-        printf("\nCadastro concluído! Obrigado por usar o sistema.\n"); 
+    printf("Digite o nome da cidade: ");
+    scanf("%s", cidade);
+
+    printf("Digite o estado: ");
+    scanf("%s", estado);
+
+    printf("Digite a população da cidade: ");
+    scanf("%d", &populacao);
+
+    printf("Digite a área do da cidade em km²: ");
+    scanf("%f", &area);
+
+    printf("Digite o PIB da cidade em bilhões: ");
+    scanf("%lf", &PIB);
+
+    printf("Digite o número de pontos turísticos da cidade: ");
+    scanf("%d", &pontosturisticos);
+
+    printf("\n--- Carta cadastrada: %s ---\n", codigo);
+    
+    printf("Seu Páis é: %s\n", pais);
+    printf("Cidade: %s\n", cidade);
+    printf("Estado: %s\n", estado);
+    printf("População: %d\n", populacao);
+    printf("Área: %.2f km²\n", area);
+    printf("PIB: %.2lf bilhões\n", PIB);
+    printf("Pontos turísticos: %d\n", pontosturisticos);
+    printf("A Densidade populacional é de: %.2f habitantes por km²\n", (float) populacao / area); // A densidade populacional é a população dividida pela área do país.
+    printf("O PIB per capita é de: %.2lf bilhões\n", PIB / populacao); // O PIB per capita é o PIB dividido pela população da cidade.
+    
+    printf("\nCarta cadastrada com sucesso, ");
+    printf("vamos cadastrar a segunda carta!\n");
+
+    printf("\nDigite o codigo país (exemplo: A01): ");
+    scanf("%s", codigo); // %s é para ler string, e o & é para ler o endereço de memoria.
+
+    printf("Digite o nome do país: ");
+    scanf("%s", pais);
+        
+    printf("Digite o nome da cidade: ");
+    scanf("%s", cidade);
+
+    printf("Digite o estado: ");
+    scanf("%s", estado);
+
+    printf("Digite a população da cidade: ");
+    scanf("%d", &populacao);
+
+    printf("Digite a área do da cidade em km²: ");
+    scanf("%f", &area);
+
+    printf("Digite o PIB da cidade em bilhões: ");
+    scanf("%lf", &PIB);
+
+    printf("Digite o número de pontos turísticos da cidade: ");
+    scanf("%d", &pontosturisticos);
+
+    printf("\n--- Segunda carta cadastrada com sucesso!: %s ---\n", codigo);
+    
+    printf("Seu Páis é: %s\n", pais);
+    printf("Cidade: %s\n", cidade);
+    printf("Estado: %s\n", estado);
+    printf("População: %d\n", populacao);
+    printf("Área: %.2f km²\n", area);
+    printf("PIB: %.2lf bilhões\n", PIB);
+    printf("Pontos turísticos: %d\n", pontosturisticos);
+    printf("A Densidade populacional é de: %.2f habitantes por km²\n", (float) populacao / area); // A densidade populacional é a população dividida pela área do país.
+    printf("O PIB per capita é de: %.2lf bilhões\n", PIB / populacao); // O PIB per capita é o PIB dividido pela população da cidade.
+     
+    printf("Cadastro concluído! Obrigado por usar o sistema.");
 
     return 0;    
-
-
-
-
-
-
-
-
-
-
 }

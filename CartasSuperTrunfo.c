7087649292029755f6b2daf3cbceaa7d96b49ab1
Biel35 <83042989+Gabrico45@users.noreply.github.com>
2025-04-08 11:25:08 -0300

    Update comparando: CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,22 +5,22 @@ int main(){
     //variaveis para a carta1
     char codigo1[4]; // Tamanho 4: 1 letra + 2 números (EX: A01) OBS: acredito que o "ENTER" conta como o 4 caracter.
     char pais1[20];
+    char estado1[20];
     char cidade1[20];
     unsigned long int populacao1;
     float area1;
     double pib1;
     int pontosturisticos1;
-    char estado1[20];
     
     //variaveis para a carta2
     char codigo2[4];
     char pais2[20];
+    char estado2[20]; 
     char cidade2[20];
     unsigned long int populacao2;
     float area2;
     double pib2;
     int pontosturisticos2;
-    char estado2[20]; 
     
     //variaveis para os calculos
     float densidadePopulacional1, densidadePopulacional2; // A densidade populacional é a população dividida pela área da cidade.
@@ -37,11 +37,11 @@ int main(){
     printf("Digite o nome do país: ");
     scanf(" %[^\n]", pais1); // O espaço antes do %s é para ignorar o caractere de nova linha deixado pelo scanf anterior.
         
-    printf("Digite o nome da cidade: "); 
-    scanf(" %[^\n]", cidade1); // O %[^\n] é usado para ler uma string com espaços em branco.
-
     printf("Digite o estado: ");
     scanf("%s", estado1);
+    
+    printf("Digite o nome da cidade: "); 
+    scanf(" %[^\n]", cidade1); // O %[^\n] é usado para ler uma string com espaços em branco.
 
     printf("Digite a população da cidade (milhões): ");
     scanf("%lu", &populacao1);
@@ -63,11 +63,11 @@ int main(){
     printf("Digite o nome do país: ");
     scanf(" %[^\n]", pais2); 
         
-    printf("Digite o nome da cidade: "); 
-    scanf(" %[^\n]", cidade2); 
-
     printf("Digite o estado: ");
     scanf("%s", estado2);
+    
+    printf("Digite o nome da cidade: "); 
+    scanf(" %[^\n]", cidade2); 
 
     printf("Digite a população da cidade (milhões): ");
     scanf("%lu", &populacao2);
@@ -94,23 +94,61 @@ int main(){
     SuperPoder2 = populacao2 + area2 + pib2 + pontosturisticos2 + pibPerCapita2 + (1.0f / densidadePopulacional2);
 
     // Comparação entre as cartas
-    int compPopulacao = (populacao1 > populacao2) ? 1 : 0;
-    int compArea = (area1 > area2) ? 1 : 0;
-    int compPib = (pib1 > pib2) ? 1 : 0;
-    int compPontosTuristicos = (pontosturisticos1 > pontosturisticos2) ? 1 : 0;
-    int compDensidadePopulacional = (densidadePopulacional1 < densidadePopulacional2) ? 1 : 0;
-    int compPibPerCapita = (pibPerCapita1 > pibPerCapita2) ? 1 : 0;
-    int compSuperPoder = (SuperPoder1 > SuperPoder2) ? 1 : 0;
+    if (populacao1 > populacao2) {
+        printf("\nPopulação: Carta 1 Venceu!\n");
+    } else if (populacao1 < populacao2) {
+        printf("\nPopulação: Carta 2 Venceu!\n");
+    } else {
+        printf("\nPopulação: Empate!\n");
+    }
 
-    // Exibição dos resultados das comparações
-    printf("\n=== Comparação de Cartas ===\n");
-    printf("População: Carta %d venceu (%d)\n", compPopulacao ? 1 : 2, compPopulacao);
-    printf("Área: Carta %d venceu (%d)\n", compArea ? 1 : 2, compArea);
-    printf("PIB: Carta %d venceu (%d)\n", compPib ? 1 : 2, compPib);
-    printf("Pontos Turísticos: Carta %d venceu (%d)\n", compPontosTuristicos ? 1 : 2, compPontosTuristicos);
-    printf("Densidade Populacional: Carta %d venceu (%d)\n", compDensidadePopulacional ? 1 : 2, compDensidadePopulacional);
-    printf("PIB per Capita: Carta %d venceu (%d)\n", compPibPerCapita ? 1 : 2, compPibPerCapita);
-    printf("Super Poder: Carta %d venceu (%d)\n", compSuperPoder ? 1 : 2, compSuperPoder);
+    if (area1 > area2) {
+        printf("Área: Carta 1 Venceu!\n");
+    } else if (area1 < area2) {
+        printf("Área: Carta 2 Venceu!\n");
+    } else {
+        printf("Área: Empate!\n");
+    }
+
+    if (pib1 > pib2) {
+        printf("PIB: Carta 1 Venceu!\n");
+    } else if (pib1 < pib2) {
+        printf("PIB: Carta 2 Venceu!\n");
+    } else {
+        printf("PIB: Empate!\n");
+    }
+
+    if (pontosturisticos1 > pontosturisticos2) {
+        printf("Pontos Turísticos: Carta 1 Venceu!\n");
+    } else if (pontosturisticos1 < pontosturisticos2) {
+        printf("Pontos Turísticos: Carta 2 Venceu!\n");
+    } else {
+        printf("Pontos Turísticos: Empate!\n");
+    }
+
+    if (densidadePopulacional1 < densidadePopulacional2) {
+        printf("Densidade Populacional: Carta 1 Venceu!\n");
+    } else if (densidadePopulacional1 > densidadePopulacional2) {
+        printf("Densidade Populacional: Carta 2 Venceu!\n");
+    } else {
+        printf("Densidade Populacional: Empate!\n");
+    }
+
+    if (pibPerCapita1 > pibPerCapita2) {
+        printf("PIB per Capita: Carta 1 Venceu!\n");
+    } else if (pibPerCapita1 < pibPerCapita2) {
+        printf("PIB per Capita: Carta 2 Venceu!\n");
+    } else {
+        printf("PIB per Capita: Empate!\n");
+    }
+
+    if (SuperPoder1 > SuperPoder2) {
+        printf("Super Poder: Carta 1 Venceu!\n");
+    } else if (SuperPoder1 < SuperPoder2) {
+        printf("Super Poder: Carta 2 Venceu!\n");
+    } else {
+        printf("Super Poder: Empate!\n");
+    }
 
     return 0;    
 }
